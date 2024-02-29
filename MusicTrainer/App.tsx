6eb93477e246d86c6db6532880b41d1bb2bdd32d
Ben Li <li.benjamin.b@gmail.com>
2024-02-28 23:06:48 -0500

    Add basic recording function to frontend

--- MusicTrainer/App.tsx
+++ MusicTrainer/App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Button, View } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [recording, setRecording] = useState<Audio.Recording>();
+    const [permissionResponse, requestPermission] = Audio.usePermissions();
+
+    async function startRecording() {
+        try {
+            if (permissionResponse) {
+                console.log('Requesting permission..');
+                await requestPermission();
+            }
+
+            console.log('Starting recording..');
+            const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY);
+            setRecording(recording);
+            console.log('Recording Started');
+        }catch (err) {
+            console.error('Failed to start recording', err);
+        }
+    }
+
+    async function stopRecording() {
+        console.log('Stopping recording..');
+        setRecording(undefined);
+        await recording.stopAndUnloadAsync();
+        await Audio.setAudioModeAsync(
+            {
+                allowsRecordingIOS: false,
+            }
+        );
+        const uri = recording?.getURI();
+        console.log('Recording stopped and stored at', uri);
+    }
+
+
+    return (
+        <View style={styles.container}>
+        <Button
+            title={recording ? 'Stop recording' : 'Start recording'}
+            onPress={recording ? stopRecording : startRecording}
+        />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
